railsadminteam/rails_admin

Convert dashboard to foundation

app/helpers/rails_admin/application_helper.rb
@@ -58,7 +58,7 @@ module RailsAdmin
         li_stack = navigation nodes_stack, nodes
 
         label = navigation_label || t('admin.misc.navigation')
-        %(<strong>#{label}</strong>#{li_stack}) if li_stack.present?
+        %(<li class='nav-header'>#{label}</li>#{li_stack}) if li_stack.present?
       end.join.html_safe
     end
 
@@ -91,11 +91,11 @@ module RailsAdmin
         (parent_actions ||= []) << action
       end while action.breadcrumb_parent && (action = action(*action.breadcrumb_parent)) # rubocop:disable Loop
 
-      content_tag(:ul, class: 'breadcrumb') do
+      content_tag(:nav, class: 'breadcrumbs') do
         parent_actions.collect do |a|
           am = a.send(:eval, 'bindings[:abstract_model]')
           o = a.send(:eval, 'bindings[:object]')
-          content_tag(:li, class: current_action?(a, am, o) && 'active') do
+          content_tag(:li, class: current_action?(a, am, o) && 'current') do
             crumb = if a.http_methods.include?(:get)
               link_to url_for(action: a.action_name, controller: 'rails_admin/main', model_name: am.try(:to_param), id: (o.try(:persisted?) && o.try(:id) || nil)) do
                 wording_for(:breadcrumb, a, am, o)
@@ -103,7 +103,6 @@ module RailsAdmin
             else
               content_tag(:span, wording_for(:breadcrumb, a, am, o))
             end
-            crumb += content_tag(:span, '/', class: 'divider') unless current_action?(a, am, o)
             crumb
           end
         end.reverse.join.html_safe
@@ -116,9 +115,9 @@ module RailsAdmin
       actions.collect do |action|
         wording = wording_for(:menu, action)
         %(
-          <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)}">
+          <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)} tab-title">
             <a href="#{url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
-              <i class="#{action.link_icon}"></i>
+              <i class="fa #{action.link_icon}"></i>
               <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
             </a>
           </li>

aelogica/express_rails_admin

Conflicts:
	app/helpers/rails_admin/application_helper.rb
	app/views/layouts/rails_admin/_secondary_navigation.html.haml
	app/views/layouts/rails_admin/application.html.haml

app/helpers/rails_admin/application_helper.rb
@@ -105,11 +105,11 @@ module RailsAdmin
         (parent_actions ||= []) << action
       end while action.breadcrumb_parent && (action = action(*action.breadcrumb_parent)) # rubocop:disable Loop
 
-      content_tag(:ol, class: 'breadcrumb') do
+      content_tag(:nav, class: 'breadcrumb') do
         parent_actions.collect do |a|
           am = a.send(:eval, 'bindings[:abstract_model]')
           o = a.send(:eval, 'bindings[:object]')
-          content_tag(:li, class: current_action?(a, am, o) && 'active') do
+          content_tag(:li, class: current_action?(a, am, o) && 'current') do
             crumb = begin
               if !current_action?(a, am, o)
                 if a.http_methods.include?(:get)
@@ -135,9 +135,9 @@ module RailsAdmin
       actions.collect do |action|
         wording = wording_for(:menu, action)
         %(
-          <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)}">
+          <li title="#{wording if only_icon}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)} tab-title">
             <a href="#{url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
-              <i class="#{action.link_icon}"></i>
+              <i class="fa #{action.link_icon}"></i>
               <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
             </a>
           </li>